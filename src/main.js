7ce8013575e31ce1ccfdc26197764939c5bd9382
Adam Smith <adam@adamsmith.as>
2022-04-25 15:09:16 -0700

    Playground scene with red rectangle

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
-let game = new Phaser.Game();
+class Playground extends Phaser.Scene {
+    constructor() {
+        super("playground");
+    }
+
+    create() {
+        this.add.rectangle(0,0,8,8,0xFF0000);
+    }
+}
+
+let game = new Phaser.Game({
+    width: 80,
+    height: 60,
+    zoom: 4,
+    scene: [Playground]
+});
