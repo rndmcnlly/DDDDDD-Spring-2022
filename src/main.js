1e47d1cb67e88ec536278769bb5629548f53623c
Adam Smith <adam@adamsmith.as>
2022-04-25 15:13:28 -0700

    Replace rectangle with loaded sprite assets

--- src/main.js
+++ src/main.js
@@ -3,8 +3,19 @@ class Playground extends Phaser.Scene {
         super("playground");
     }
 
+    preload() {
+        this.load.image('spike', 'assets/spike.png');
+        this.load.image('block', 'assets/block.png');
+        this.load.spritesheet('dood', 'assets/dood.png', {
+            frameWidth: 10,
+            frameHeight: 16
+        });
+    }
+
     create() {
-        this.add.rectangle(0,0,8,8,0xFF0000);
+        this.add.sprite(0,0,'spike').setOrigin(0,0);
+        this.add.sprite(0,8,'block').setOrigin(0,0);
+        this.add.sprite(8,0,'dood').setOrigin(0,0);
     }
 }
 
